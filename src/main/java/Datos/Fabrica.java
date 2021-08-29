14a2c178c3eaf3b54303becdb9dc36dbb3c7f1d4
BryanSan117 <brsandovalc@alumno.uspg.edu.gt>
2021-08-28 22:31:24 -0500

    Commit prueba

--- src/main/java/Datos/Fabrica.java
+++ src/main/java/Datos/Fabrica.java
@@ -1,11 +1,42 @@
 package Datos;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import Iconexion.iconexion;
 
 public class Fabrica {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
 		conexionfabrica Fabrica = new conexionfabrica();
+		
+		System.out.println("Introduzca una descripcion para el articulo: ");
+		String descripcion = teclado.nextLine();
+		System.out.println("Introduzca un precio para el articulo : ");
+		int precio = teclado.nextInt();
+		
+
+		try {
+			Connection conexion = DriverManager.getConnection("jdbc:sql://localhost/db_Articuloz","sa","1235");
+
+			
+			Statement consulta = (Statement) conexion.createStatement();
+			((java.sql.Statement) consulta).executeUpdate("insert into articulos (descripcion, precio) values ('"
+					+ descripcion
+					+ "',"
+					+ precio + ")");
+			
+			conexion.close();
+			System.out.println("Articulo insertado correcta mente");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
 		
 		iconexion cx1 = Fabrica.getConexion("PostgreSQL");
 		System.out.println();
